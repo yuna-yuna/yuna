d3203bbad7f3f12c588cff8f72dfb98616fc350c
yuna-yuna <45616909+yuna-yuna@users.noreply.github.com>
2018-12-17 05:18:41 +0900

    Add files via upload

---
+++ mission_3-1.php
@@ -0,0 +1,21 @@
+﻿<html>
+<head>
+<title>mission_3-1.php</title>
+</head>
+<body>
+
+<?php
+$dsn = 'mysql:dbname=データベース名;host=localhost';
+$user = 'ユーザー名';
+$password = 'パスワード';
+
+try{
+$pdo=new PDO($dsn,$user,$password);
+array(PDO::ATTR_EMULATE_PREPARES => false));
+} catch (PDOException $e) {\\\
+ exit('データベース接続失敗。'.$e->getMessage());
+}
+
+?>
+</body>
+</html>
